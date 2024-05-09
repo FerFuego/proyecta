0707e087668339296115613341e32c37925f2cf9
Fer Catalano <fernando@hazak.tech>
2024-05-09 20:39:25 -0300

    Add - Site Name

--- inc/layout/head.php
+++ inc/layout/head.php
@@ -25,15 +25,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="title" content="NUESTRO Polirrubros - Mayorista de juguetes, libreria, regalaria, bazar, marroquineria y ropa interior">
-    <meta name="keywords" content="polirrubros, polirubros, nuestro polirrubros, mayorista, venta, juguetes, libreria, regalaria, bazar, marroquineria, ropa interior, bell ville, cordoba, argentina">
-    <meta name="description" content="NUESTRO Polirrubros de Alejandra Barzabal - Mayorista de juguetes, libreria, regalaria, bazar, marroquineria y ropa interior">
+    <meta name="title" content="Proyecta Materiales">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
     <meta name="author" content="Luciano Colmano y Fernando Catalano">
     <meta name="Robots" content="All">
     <meta name="Revisit-after" content="15 days">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="img/favicon.png">
-    <title>Nuestro Polirrubros | Bell Ville</title>
+    <title>Proyecta Materiales | Bell Ville</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
