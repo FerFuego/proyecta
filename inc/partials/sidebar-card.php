7e1ba7ae77a96b65e54899f316d7b60fa12ef512
Luciano Colmano <lcolmano@gmail.com>
2024-09-09 19:22:52 -0300

    para acceder a ficha producto, busqueda por Id_Producto

--- inc/partials/sidebar-card.php
+++ inc/partials/sidebar-card.php
@@ -1,5 +1,5 @@
 <div class="latest-prdouct__slider__item">
-    <a href="detalle.php?id=<?php echo $product->CodProducto; ?>" class="latest-product__item">
+    <a href="detalle.php?id=<?php echo $product->Id_Producto; ?>" class="latest-product__item">
         <div class="latest-product__item__text">
             <img src="<?php echo Productos::getImage( $product->CodProducto ); ?>" alt="<?php echo $product->CodProducto; ?>">
             <h6><?php echo $product->Nombre; ?></h6>
