83cd91be45deb7eb8048ff09cfd786246636e2a1
Luciano Colmano <lcolmano@gmail.com>
2024-09-09 19:50:45 -0300

    edito banner shopping

--- inc/parts/banner-shopping.php
+++ inc/parts/banner-shopping.php
@@ -4,22 +4,22 @@
             <div class="banner-shopping__item col-lg-3 col-md-6 col-xs-12">
                 <i class="fa fa-shopping-cart fa-4x"></i>
                 <h4 class="banner-shopping__title">REALIZÁ TU PEDIDO</h4>
-                <p class="banner-shopping__text">El monto mínimo de compra es de $50.000. Agrega al carrito los productos que quieras y completa los datos personales.</p>
+                <p class="banner-shopping__text">Muy pronto habilitaremos el carrito del compras en el sitio web. Por el momento, ponete en contacto por WhatsApp para realizar tu pedido.</p>
             </div>
             <div class="banner-shopping__item col-lg-3 col-md-6 col-xs-12">
                 <i class="fa fa-whatsapp fa-4x"></i>
                 <h4 class="banner-shopping__title">ARMAMOS TU PEDIDO</h4>
-                <p class="banner-shopping__text">Nos pondremos en contacto con vos dentro de las 24hs para confirmar stock.</p>
+                <p class="banner-shopping__text">Nos pondremos en contacto con vos dentro de las 24 hs para confirmar stock.</p>
             </div>
             <div class="banner-shopping__item col-lg-3 col-md-6 col-xs-12">
                 <i class="fa fa-credit-card fa-4x"></i>
                 <h4 class="banner-shopping__title">REALIZÁ EL PAGO</h4>
-                <p class="banner-shopping__text">Podes abonar por depósito bancario, transferencia o en efectivo en nuestro local.</p>
+                <p class="banner-shopping__text">Podes abonar por depósito bancario, transferencia, tarjeta de crédito o débito y efectivo en nuestro local.</p>
             </div>
             <div class="banner-shopping__item col-lg-3 col-md-6 col-xs-12">
                 <i class="fa fa-truck fa-4x"></i>
                 <h4 class="banner-shopping__title">ENTREGA DE TU PEDIDO</h4>
-                <p class="banner-shopping__text">Retira tu pedido en nuestro local o te lo enviamos por encomienda.</p>
+                <p class="banner-shopping__text">Retira tu pedido en nuestro local o te lo enviamos con nuestro servicio de reparto.</p>
             </div>
         </div>
     </div>
